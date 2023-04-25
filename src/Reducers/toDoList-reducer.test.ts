efb78388fb74be9fff797a99bd9995c4585ef6ab
Indifferent98 <zhenyayarmokova@mail.ru>
2023-04-25 23:44:32 +0300

    add all missing function to reducer and cover test

--- src/Reducers/toDoList-reducer.test.ts
+++ src/Reducers/toDoList-reducer.test.ts
@@ -27,3 +27,97 @@ test("DELETE TO DO LIST TEST", () => {
   expect(result.length).toBe(1);
   expect(state.length).toBe(2);
 });
+
+test("ADD-NEW-TO-DO-LIST test", () => {
+  const toDoListId_1 = v1();
+  const toDoListId_2 = v1();
+  const state: toDolistType[] = [
+    {
+      id: toDoListId_1,
+      filter: "all",
+      title: "What to buy",
+    },
+    {
+      filter: "all",
+      id: toDoListId_2,
+      title: "What to learn",
+    },
+  ];
+
+  const result = toDoListReducer(state, {
+    type: "ADD-NEW-TO-DO-LIST",
+    title: "NEWW TITLE",
+  });
+
+  expect(result.length).toBe(3);
+  expect(state.length).toBe(2);
+  expect(result[2].title).toBe("NEWW TITLE");
+  expect(result[1].title).toBe("What to learn");
+  expect(state).not.toBe(result);
+  expect(state).not.toEqual(result);
+});
+
+test("CHANGE-HEADDER-TITLE", () => {
+  const toDoListId_1 = v1();
+  const toDoListId_2 = v1();
+  const state: toDolistType[] = [
+    {
+      id: toDoListId_1,
+      filter: "all",
+      title: "What to buy",
+    },
+    {
+      filter: "all",
+      id: toDoListId_2,
+      title: "What to learn",
+    },
+  ];
+
+  const result = toDoListReducer(state, {
+    type: "CHANGE-HEADDER-TITLE",
+    title: "qqqq QQQQ",
+    toDoListId: toDoListId_2,
+  });
+
+  expect(result.length).toBe(2);
+  expect(state.length).toBe(2);
+  expect(result[1].title).toBe("qqqq QQQQ");
+  expect(result[0].title).toBe("What to buy");
+  expect(state).not.toBe(result);
+  expect(state).not.toEqual(result);
+  expect(state[1].title).toBe("What to learn");
+  expect(state[0].title).toBe("What to buy");
+});
+
+test("CHANGE-FILTER", () => {
+  const toDoListId_1 = v1();
+  const toDoListId_2 = v1();
+  const state: toDolistType[] = [
+    {
+      id: toDoListId_1,
+      filter: "all",
+      title: "What to buy",
+    },
+    {
+      filter: "all",
+      id: toDoListId_2,
+      title: "What to learn",
+    },
+  ];
+
+  const result = toDoListReducer(state, {
+    type: "CHANGE-FILTER",
+
+    toDoListId: toDoListId_2,
+    filter: "completed",
+  });
+
+  expect(result.length).toBe(2);
+  expect(state.length).toBe(2);
+  expect(result[1].filter).toBe("completed");
+  expect(state[1].filter).toBe("all");
+  expect(result[0].filter).toBe("all");
+  expect(state[0].filter).toBe("all");
+  expect(state).not.toBe(result);
+  expect(state).not.toEqual(result);
+});
--- src/Reducers/toDoList-reducer.ts
+++ src/Reducers/toDoList-reducer.ts
@@ -1,11 +1,34 @@
+import { filterType } from "./../components/ToDoList";
 import React from "react";
 import { toDolistType } from "../App";
+import { v1 } from "uuid";
 
-type ActionsType = DeleteToDoListActionType;
+type ActionsType =
+  | DeleteToDoListActionType
+  | AddToDoListActionType
+  | ChangeHeadderTitleActionType
+  | changeFilterActionType;
 
 type DeleteToDoListActionType = {
   type: "DELETE-TO-DO-LIST";
   toDoListId: string;
+};
+
+type AddToDoListActionType = {
+  type: "ADD-NEW-TO-DO-LIST";
+  title: string;
+};
+
+type ChangeHeadderTitleActionType = {
+  title: string;
+  toDoListId: string;
+  type: "CHANGE-HEADDER-TITLE";
+};
+
+type changeFilterActionType = {
+  filter: filterType;
+  toDoListId: string;
+  type: "CHANGE-FILTER";
 };
 
 export const toDoListReducer = (
@@ -15,6 +38,25 @@ export const toDoListReducer = (
   switch (action.type) {
     case "DELETE-TO-DO-LIST":
       return state.filter((t) => t.id !== action.toDoListId);
+
+    case "ADD-NEW-TO-DO-LIST":
+      const newToDoListId = v1();
+      const newToDoList: toDolistType = {
+        id: newToDoListId,
+        filter: "all",
+        title: action.title,
+      };
+      return [...state, newToDoList];
+
+    case "CHANGE-HEADDER-TITLE":
+      return state.map((t) =>
+        t.id === action.toDoListId ? { ...t, title: action.title } : t
+      );
+
+    case "CHANGE-FILTER":
+      return state.map((t) =>
+        t.id === action.toDoListId ? { ...t, filter: action.filter } : t
+      );
 
     default:
       throw new Error("Bad action type");
