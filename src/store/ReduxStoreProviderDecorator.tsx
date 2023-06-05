eb8c25c5343751a7a4a61a403543d842ab1d0a2c
Indifferent98 <zhenyayarmokova@mail.ru>
2023-06-05 21:27:58 +0300

    add hoc stories decorator

--- src/store/ReduxStoreProviderDecorator.tsx
+++ src/store/ReduxStoreProviderDecorator.tsx
@@ -1,7 +1,37 @@
 import React from "react";
 import { Provider } from "react-redux";
-import { store } from "./Store";
+import { AppRootStateType, store } from "./Store";
+import { toDolistType, useStateTaskType } from "../AppWithRedux";
+import { combineReducers, legacy_createStore } from "redux";
+import { taskReducer } from "../Reducers/task-reducer";
+import { toDoListReducer } from "../Reducers/toDoList-reducer";
 
 export const ReduxStoreProviderDecorator = (storyFn: () => React.ReactNode) => {
-  return <Provider store={store}>{storyFn()}</Provider>;
+  const rootReducer = combineReducers({
+    task: taskReducer,
+    toDoList: toDoListReducer,
+  });
+  const initialGlobalState = {
+    task: {
+      "123": [
+        { id: "4444", title: "React", isDone: true },
+        { id: "666", title: "Redux", isDone: false },
+      ],
+      "333": [
+        { title: "Salt", id: "32423", isDone: true },
+        { title: "Water", id: "ergdf", isDone: false },
+      ],
+    },
+    toDoList: [
+      { id: "123", filter: "all", title: "what to buy" },
+      { id: "333", filter: "all", title: "what to learn" },
+    ],
+  };
+
+  const storyBookStore = legacy_createStore(
+    rootReducer,
+    initialGlobalState as AppRootStateType
+  );
+
+  return <Provider store={storyBookStore}>{storyFn()}</Provider>;
 };
