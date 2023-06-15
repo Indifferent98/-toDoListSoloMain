bb3fb1af36306ce7415fa230e4489a4cab1ec9eb
Indifferent98 <zhenyayarmokova@mail.ru>
2023-06-15 21:30:10 +0300

    add base api requests

--- src/stories/todolists-api.stories.tsx
+++ src/stories/todolists-api.stories.tsx
@@ -1,37 +1,158 @@
-import React, { useEffect, useState } from "react";
+import { dividerClasses } from "@mui/material";
+import axios from "axios";
+import React, { ChangeEvent, useEffect, useState } from "react";
 
 export default {
   title: "API",
 };
 
+const settings = {
+  withCredentials: true,
+  headers: {
+    "API-KEY": "2ca10f7e-8d65-43a0-a381-2411fab70789",
+  },
+};
+
+const apiBaseUrl = "https://social-network.samuraijs.com/api/1.1/";
+
 export const GetToDoLists = () => {
   const [state, setState] = useState<any>(null);
 
-  useEffect(() => {}, []);
+  useEffect(() => {
+    axios.get(apiBaseUrl + "todo-lists", settings).then((res) => {
+      debugger;
+      setState(res.data);
+    });
+  }, []);
 
-  return <div>{JSON.stringify(state)}</div>;
+  return (
+    <div>
+      {state ? state.map((t: any) => <div>{JSON.stringify(t)}</div>) : null}
+    </div>
+  );
 };
 
 export const CreateToDoList = () => {
   const [state, setState] = useState<any>(null);
 
-  useEffect(() => {}, []);
+  // useEffect(() => {
+  //   axios
+  //     .post(
+  //       apiBaseUrl + "todo-lists",
+  //       {
+  //         title: "hello",
+  //       },
+  //       settings
+  //     )
+  //     .then((res) => setState(res.data));
+  // }, []);
 
-  return <div>{JSON.stringify(state)}</div>;
+  const createToDo = () => {
+    axios
+      .post(
+        apiBaseUrl + "todo-lists",
+        {
+          title: todoListTitle,
+        },
+        settings
+      )
+      .then((res) => setState(res.data));
+    setToDoListTitle("");
+  };
+  const [todoListTitle, setToDoListTitle] = useState("");
+
+  const onchangehandler = (e: ChangeEvent<HTMLInputElement>) => {
+    setToDoListTitle(e.currentTarget.value);
+  };
+  return (
+    <div>
+      <div>
+        <input
+          onChange={onchangehandler}
+          type="text"
+          placeholder="title"
+          value={todoListTitle}
+        />
+        <button onClick={createToDo}>+</button>
+      </div>
+      {JSON.stringify(state)}
+    </div>
+  );
 };
 
 export const UpdateToDoListTitle = () => {
   const [state, setState] = useState<any>(null);
 
-  useEffect(() => {}, []);
+  // useEffect(() => {}, []);
+  const [todolistid, setTodolistid] = useState("");
+  const [newTodolistTitle, setNewTodolistTitle] = useState("");
+  const updToDoListTitle = () => {
+    axios
+      .put(
+        apiBaseUrl + "todo-lists/" + todolistid,
+        { title: newTodolistTitle },
+        settings
+      )
+      .then((res) => setState(res));
+  };
 
-  return <div>{JSON.stringify(state)}</div>;
+  const onchangehandler1 = (e: ChangeEvent<HTMLInputElement>) => {
+    setTodolistid(e.currentTarget.value);
+  };
+  const onchangehandler2 = (e: ChangeEvent<HTMLInputElement>) => {
+    setNewTodolistTitle(e.currentTarget.value);
+  };
+
+  return (
+    <div>
+      <div>
+        <input
+          type="text"
+          placeholder="todolistid"
+          value={todolistid}
+          onChange={onchangehandler1}
+        />
+        <input
+          type="text"
+          placeholder="newToDoListTitle"
+          value={newTodolistTitle}
+          onChange={onchangehandler2}
+        />
+        <button onClick={updToDoListTitle}>+</button>
+      </div>
+      {JSON.stringify(state)}
+    </div>
+  );
 };
 
 export const DeleteToDoList = () => {
   const [state, setState] = useState<any>(null);
 
-  useEffect(() => {}, []);
+  // useEffect(() => {}, []);
+  const delList = () => {
+    axios
+      .delete(apiBaseUrl + "todo-lists/" + todolistid, settings)
+      .then((res) => setState(res));
 
-  return <div>{JSON.stringify(state)}</div>;
+    setTodolistid("");
+  };
+  const onchangeHandler = (e: ChangeEvent<HTMLInputElement>) => {
+    setTodolistid(e.currentTarget.value);
+  };
+  const [todolistid, setTodolistid] = useState("");
+
+  return (
+    <div>
+      <div>
+        <input
+          onChange={onchangeHandler}
+          type="text"
+          placeholder="todolistid"
+          value={todolistid}
+        />{" "}
+        <button onClick={delList}>+</button>
+      </div>
+      {JSON.stringify(state)}
+    </div>
+  );
 };
